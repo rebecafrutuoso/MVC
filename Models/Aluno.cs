8683820b45f69f77f6b8112bb058b5b2d0c64904
rebecafrutuoso <108592549+rebecafrutuoso@users.noreply.github.com>
2022-11-15 23:59:02 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Aluno
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+    }
+}
